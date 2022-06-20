4e210f44c58391b969a75c47cc00951348d7df4b
Pratikshya <pratikshya@cybrain.co.in>
2022-06-20 11:42:38 +0530

    practive project

---
+++ TestingProject/src/main/java/TestingProject/TestingProject/App.java
@@ -0,0 +1,13 @@
+package TestingProject.TestingProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
